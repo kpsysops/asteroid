e780c51c5e3e3d05a37ce9bf97f791adff4525a4
kpsysops <kp.sysops@gmail.com>
2024-12-15 10:00:43 +0100

    Adding grouping

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from circleshape import *
 
 def main():
     pygame.init() # Init
-
+    updateble = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    Player.containers = (updateble, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock() 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -17,11 +19,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
-        player.update(dt)
+        for update in updateble:
+            update.update(dt)
         screen.fill((0,0,0))
-        player.draw(screen)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
-        
         dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
